af35806e7a45bfc85bedb75626b2b5b4eae95b4e
vladyslavhimself <vladyslavhimself@gmail.com>
2025-03-08 19:12:34 -0500

    Added possibility to exit modal via hotkey

--- src/components/AuthModals/ModalLayout/ModalLayout.tsx
+++ src/components/AuthModals/ModalLayout/ModalLayout.tsx
@@ -2,11 +2,23 @@
 
 import styles from './modalLayout.module.scss';
 import {useRouter} from "next/navigation";
-import React from "react";
+import React, {useEffect} from "react";
 
 
 export default function ModalLayout({ children, modalName}: { children: React.ReactNode, modalName: string}) {
     const router = useRouter();
+
+    useEffect(() => {
+        function inspectCloseHotkey(e: KeyboardEvent) {
+            if (e.code === 'Escape') {
+                router.back();
+            }
+        }
+
+        window.addEventListener('keydown', inspectCloseHotkey);
+
+        return () => window.removeEventListener('keydown', inspectCloseHotkey)
+    }, [router]);
 
     return (
         <div className={styles['modal-backdrop']} onClick={router.back}>
