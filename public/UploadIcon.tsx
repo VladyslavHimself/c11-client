ffe09b5eb80dc641a5bcd9d4a0592b137a3675ae
vladyslavhimself <vladyslavhimself@gmail.com>
2025-04-08 16:50:55 -0400

    Added UploadIcon to previous commit
    Signed-off-by: vladyslavhimself <vladyslavhimself@gmail.com>

---
+++ public/UploadIcon.tsx
@@ -0,0 +1,13 @@
+
+
+export default function UploadIcon() {
+    return (
+        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 42C14.06 42 6 33.94 6 24C6 14.06 14.06 6 24 6" stroke="#808080" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="1.58 3.17"/>
+            <path d="M24 6C33.94 6 42 14.06 42 24C42 33.94 33.94 42 24 42" stroke="#808080" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M24 32V17" stroke="#808080" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M31 24L24 17L17 24" stroke="#808080" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+
+    );
+};
