658d24226becd308b32f3c1b96cd5d4e04ddf626
vladyslavhimself <vladyslavhimself@gmail.com>
2025-04-17 20:56:22 -0400

    Protect unauthorized access to pages by direct links
    Signed-off-by: vladyslavhimself <vladyslavhimself@gmail.com>

--- src/app/(global)/add-new/layout.tsx
+++ src/app/(global)/add-new/layout.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import {AddNewPageProviders} from "@/components/AddNewPage/AddNewPageProviders";
 import {TopicsAPI} from "@/api/Topics";
 import {TagsApi} from "@/api/Tags";
+import {getServerSession} from "next-auth";
+import {authConfig} from "@/config/authConfig";
+import {redirect} from "next/navigation";
 
 export default async function AddNewPageLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const session = await getServerSession(authConfig);
+
+    if (!session?.token) {
+        redirect('/')
+    }
+
     const topicList = await TopicsAPI.getPopularTopics(4);
     const tagList = await TagsApi.searchTags(undefined, 9);
 
